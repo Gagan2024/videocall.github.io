197e86cd6f6c5a9cc1604b12e2e26bfd066ae922
Gagan Srivastava <71887687+Gagan2024@users.noreply.github.com>
2022-03-11 09:19:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let client = AgoraRTC.createClient({mode:'rtc', codec:"vp8"})
 
 //#2
 let config = {
-    appid:'665ee9b434d547c5a2e23c22ea420241',
+    appid:'d08bfe51970549219d4b0ccce035fc27',
     token:'006d08bfe51970549219d4b0ccce035fc27IAAqQu1cHr5l6vvAZnNLeMN4z3QkQWXtoIr3O01gQtFxUp1fCyoAAAAAEACciVDW6BYsYgEAAQDoFixi',
     uid:null,
     channel:'livestream',
